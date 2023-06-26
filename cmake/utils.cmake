8bb801dd951e0f3fb41afb2bc6e0b689be29acdb
weicaixin <weicaixin@qq.com>
2023-06-26 17:35:34 +0800

    ...

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,89 +1,89 @@
-function(utils_add_lib)
-  cmake_parse_arguments(ARGS "" "TARGET;TYPE;OUTPUT_NAME;SRC_DIR;INC_DIR" "DEPS" ${ARGN})
-  message(STATUS "==== util_add_lib  ====")
-  message(STATUS "TARGET        : ${ARGS_TARGET}")
-  message(STATUS "TYPE          : ${ARGS_TYPE}")
-  message(STATUS "ARGS_SRC_DIR  : ${ARGS_SRC_DIR}")
-  message(STATUS "ARGS_INC_DIR  : ${ARGS_INC_DIR}")
-  message(STATUS "ARGS_DEPS     : ${ARGS_DEPS}")
-  message(STATUS "OUTPUT_NAME   : ${ARGS_OUTPUT_NAME}")
+function(utils_add_lib ARG_TARGET ARG_TYPE)
+  cmake_parse_arguments(ARG "" "SRC_DIR;INC_DIR;OUTPUT_NAME" "DEPS" ${ARGN})
+  message(STATUS "==== util_add_lib ====")
+  message(STATUS "TARGET      : ${ARG_TARGET}")
+  message(STATUS "TYPE        : ${ARG_TYPE}")
+  message(STATUS "SRC_DIR     : ${ARG_SRC_DIR}")
+  message(STATUS "INC_DIR     : ${ARG_INC_DIR}")
+  message(STATUS "DEPS        : ${ARG_DEPS}")
+  message(STATUS "OUTPUT_NAME : ${ARG_OUTPUT_NAME}")
 
-  file(GLOB_RECURSE files ${ARGS_SRC_DIR}/*.cc ${ARGS_SRC_DIR}/*.h)
+  file(GLOB_RECURSE files ${ARG_SRC_DIR}/*.cc ${ARG_SRC_DIR}/*.h)
   list(APPEND src_files ${files})
 
-  add_library(${ARGS_TARGET} ${ARGS_TYPE} ${src_files})
-  target_link_libraries(${ARGS_TARGET} PRIVATE ${ARGS_DEPS})
-  target_include_directories(${ARGS_TARGET} PUBLIC
+  add_library(${ARG_TARGET} ${ARG_TYPE} ${src_files})
+  target_link_libraries(${ARG_TARGET} PRIVATE ${ARG_DEPS})
+  target_include_directories(${ARG_TARGET} PUBLIC
     "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>"
-    "$<BUILD_INTERFACE:${ARGS_INC_DIR}>"
+    "$<BUILD_INTERFACE:${ARG_INC_DIR}>"
     "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>"
   )
-  if (ARGS_OUTPUT_NAME)
-    set_property(TARGET ${ARGS_TARGET} PROPERTY OUTPUT_NAME ${ARGS_OUTPUT_NAME})
-  endif(ARGS_OUTPUT_NAME)
+  if (ARG_OUTPUT_NAME)
+    set_property(TARGET ${ARG_TARGET} PROPERTY OUTPUT_NAME ${ARG_OUTPUT_NAME})
+  endif(ARG_OUTPUT_NAME)
 
   include(GenerateExportHeader)
-  generate_export_header(${ARGS_TARGET})
-
+  generate_export_header(${ARG_TARGET})
   message(STATUS "Done")
 endfunction(utils_add_lib)
 
 
-function(utils_add_exec)
-  cmake_parse_arguments(ARGS "" "TARGET;OUTPUT_NAME;SRC_DIR" "DEPS" ${ARGN})
-  message(STATUS "==== util_add_exec  ====")
-  message(STATUS "TARGET        : ${ARGS_TARGET}")
-  message(STATUS "TYPE          : ${ARGS_TYPE}")
-  message(STATUS "ARGS_SRC_DIR  : ${ARGS_SRC_DIR}")
-  message(STATUS "ARGS_DEPS     : ${ARGS_DEPS}")
-  message(STATUS "OUTPUT_NAME   : ${ARGS_OUTPUT_NAME}")
+function(utils_add_exec ARG_TARGET)
+  cmake_parse_arguments(ARG "" "SRC_DIR;OUTPUT_NAME" "DEPS" ${ARGN})
+  message(STATUS "==== util_add_exec ====")
+  message(STATUS "TARGET      : ${ARG_TARGET}")
+  message(STATUS "SRC_DIR     : ${ARG_SRC_DIR}")
+  message(STATUS "DEPS        : ${ARG_DEPS}")
+  message(STATUS "OUTPUT_NAME : ${ARG_OUTPUT_NAME}")
 
-  file(GLOB_RECURSE files ${ARGS_SRC_DIR}/*.cc ${ARGS_SRC_DIR}/*.h)
+  file(GLOB_RECURSE files ${ARG_SRC_DIR}/*.cc ${ARG_SRC_DIR}/*.h)
   list(APPEND src_files ${files})
 
-  add_executable(${ARGS_TARGET} ${src_files})
-  target_link_libraries(${ARGS_TARGET} PRIVATE ${ARGS_DEPS})
-  if (ARGS_OUTPUT_NAME)
-    set_property(TARGET ${ARGS_TARGET} PROPERTY OUTPUT_NAME ${ARGS_OUTPUT_NAME})
-  endif(ARGS_OUTPUT_NAME)
+  add_executable(${ARG_TARGET} ${src_files})
+  target_link_libraries(${ARG_TARGET} PRIVATE ${ARG_DEPS})
+  if (ARG_OUTPUT_NAME)
+    set_property(TARGET ${ARG_TARGET} PROPERTY OUTPUT_NAME ${ARG_OUTPUT_NAME})
+  endif(ARG_OUTPUT_NAME)
 
   message(STATUS "Done")
 endfunction(utils_add_exec)
 
 
-function(utils_install)
-  cmake_parse_arguments(ARGS "" "PACKAGE" "TARGETS" ${ARGN})
-  message(STATUS "==== util_install ${ARGS_PACKAGE} ====")
-  message(STATUS "ARGS_PACKAGE : ${ARGS_PACKAGE}")
-  message(STATUS "ARGS_TARGETS : ${ARGS_TARGETS}")
+function(utils_install ARG_PACKAGE)
+  cmake_parse_arguments(ARG "" "VERSION;CONFIG" "TARGETS" ${ARGN})
+  message(STATUS "==== util_install ====")
+  message(STATUS "PACKAGE : ${ARG_PACKAGE}")
+  message(STATUS "VERSION : ${ARG_VERSION}")
+  message(STATUS "CONFIG  : ${ARG_CONFIG}")
+  message(STATUS "TARGETS : ${ARG_TARGETS}")
 
-  install(TARGETS ${ARGS_TARGETS} EXPORT ${ARGS_PACKAGE}
+  install(TARGETS ${ARG_TARGETS} EXPORT ${ARG_PACKAGE}
           LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
           ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
           RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
           INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
   )
   
-  install(EXPORT ${ARGS_PACKAGE} FILE ${ARGS_PACKAGE}.cmake DESTINATION shared/${ARGS_PACKAGE})
+  install(EXPORT ${ARG_PACKAGE} FILE ${ARG_PACKAGE}.cmake DESTINATION shared/${ARG_PACKAGE})
 
   include(CMakePackageConfigHelpers)
-  configure_package_config_file(${CMAKE_CURRENT_SOURCE_DIR}/Config.cmake.in
-    "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_PACKAGE}Config.cmake"
-    INSTALL_DESTINATION shared/${ARGS_PACKAGE}
+  configure_package_config_file(${ARG_CONFIG}
+    "${CMAKE_CURRENT_BINARY_DIR}/${ARG_PACKAGE}Config.cmake"
+    INSTALL_DESTINATION shared/${ARG_PACKAGE}
     NO_SET_AND_CHECK_MACRO
     NO_CHECK_REQUIRED_COMPONENTS_MACRO
   )
 
   write_basic_package_version_file(
-    "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_PACKAGE}ConfigVersion.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/${ARG_PACKAGE}ConfigVersion.cmake"
     VERSION "0.0.1"
     COMPATIBILITY AnyNewerVersion
   )
 
   install(FILES
-    ${CMAKE_CURRENT_BINARY_DIR}/${ARGS_PACKAGE}Config.cmake
-    ${CMAKE_CURRENT_BINARY_DIR}/${ARGS_PACKAGE}ConfigVersion.cmake
-    DESTINATION shared/${ARGS_PACKAGE}
+    ${CMAKE_CURRENT_BINARY_DIR}/${ARG_PACKAGE}Config.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/${ARG_PACKAGE}ConfigVersion.cmake
+    DESTINATION shared/${ARG_PACKAGE}
   )
 
   message(STATUS "Done")
